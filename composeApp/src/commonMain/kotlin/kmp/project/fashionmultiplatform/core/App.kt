38eb93ddda9037a99d1b9d7effd93b98e2fc2395
islombeknishonboev <nishonboevislombek@gmail.com>
2025-03-24 10:46:05 +0500

    feat : FullScreen enabled

--- composeApp/src/commonMain/kotlin/kmp/project/fashionmultiplatform/core/App.kt
+++ composeApp/src/commonMain/kotlin/kmp/project/fashionmultiplatform/core/App.kt
@@ -1,66 +1,31 @@
 package kmp.project.fashionmultiplatform.core
 
-import androidx.compose.foundation.border
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.calculateEndPadding
+import androidx.compose.foundation.layout.calculateStartPadding
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
+import androidx.compose.ui.unit.LayoutDirection
+import kmp.project.fashionmultiplatform.entrance.presentation.EntranceScreen
 import kmp.project.fashionmultiplatform.ui.theme.FashionTheme
 
 @Composable
 fun App() {
     FashionTheme {
-        Scaffold(
-            modifier = Modifier.fillMaxWidth(),
-            containerColor = MaterialTheme.colorScheme.background
-        ) {
-            Column(
-                modifier = Modifier.fillMaxWidth().padding(it)
-            ) {
-                Text("text", style = MaterialTheme.typography.displayLarge)
-                BasicListScreen()
-            }
-        }
-    }
-}
-
-
-@Composable
-fun BasicListScreen(modifier: Modifier = Modifier) {
-    LazyColumn(
-        modifier = modifier
-            .fillMaxWidth()
-            .padding(start = 10.dp, end = 10.dp),
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        items(100) {
-            Text(
-                text = it.toString(),
-                fontSize = 24.sp,
-                textAlign = TextAlign.Center,
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(top = 10.dp)
-                    .border(
-                        1.dp,
-                        Color.Red,
-                        shape = RoundedCornerShape(6.dp)
-                    )
+        Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+            EntranceScreen(
+                modifier = Modifier.padding(
+                    top = innerPadding.calculateTopPadding(),
+                    start = innerPadding.calculateStartPadding(LayoutDirection.Ltr),
+                    end = innerPadding.calculateEndPadding(LayoutDirection.Ltr)
+                )
             )
         }
     }
 }
+
 
 
 
--- composeApp/src/iosMain/kotlin/kmp/project/fashionmultiplatform/MainViewController.kt
+++ composeApp/src/iosMain/kotlin/kmp/project/fashionmultiplatform/MainViewController.kt
@@ -1,14 +1,16 @@
 package kmp.project.fashionmultiplatform
 
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Surface
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.window.ComposeUIViewController
 import kmp.project.fashionmultiplatform.core.App
 import kmp.project.fashionmultiplatform.ui.theme.FashionTheme
 
-fun MainViewController() = ComposeUIViewController {
+@Suppress("unused", "FunctionName")
+fun MainViewController(
+) = ComposeUIViewController {
     FashionTheme {
         Surface(
             color = MaterialTheme.colorScheme.background,
@@ -17,5 +19,4 @@ fun MainViewController() = ComposeUIViewController {
             App()
         }
     }
-
 }
