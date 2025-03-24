5456b16516e707f4c8325030d08e21b8eb72ff13
islombeknishonboev <nishonboevislombek@gmail.com>
2025-03-24 10:46:45 +0500

    feat : new resources added

--- composeApp/src/commonMain/kotlin/kmp/project/fashionmultiplatform/ui/theme/Color.kt
+++ composeApp/src/commonMain/kotlin/kmp/project/fashionmultiplatform/ui/theme/Color.kt
@@ -2,8 +2,8 @@ package kmp.project.fashionmultiplatform.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val background = Color(0XFF37FF00)
-val onBackground = Color(0XFFFFFFFF)
+val background = Color(0XFFEEEEEE)
+val onBackground = Color(0XFF000000)
 val primaryContainer = Color(0XFFFFFFFF)
 val onPrimaryContainer = Color(0XFFFFFFFF)
 val primary = Color(0XFF000000)
