819ed0ce83208e14f78b05df209da84443bfd2ae
islombeknishonboev <nishonboevislombek@gmail.com>
2025-03-27 20:29:20 +0500

    feat: Product Screen added

--- composeApp/src/commonMain/kotlin/kmp/project/fashionmultiplatform/core/App.kt
+++ composeApp/src/commonMain/kotlin/kmp/project/fashionmultiplatform/core/App.kt
@@ -8,14 +8,14 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.LayoutDirection
-import kmp.project.fashionmultiplatform.entrance.presentation.EntranceScreen
+import kmp.project.fashionmultiplatform.products.ProductsScreen
 import kmp.project.fashionmultiplatform.ui.theme.FashionTheme
 
 @Composable
 fun App() {
     FashionTheme {
         Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-            EntranceScreen(
+            ProductsScreen(
                 modifier = Modifier.padding(
                     top = innerPadding.calculateTopPadding(),
                     start = innerPadding.calculateStartPadding(LayoutDirection.Ltr),
